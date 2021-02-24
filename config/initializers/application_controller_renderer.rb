ba2b35ab2d8e51c601c31c96e4b77399b2d1171b
ume388 <turi388ouji@gmail.com>
2021-02-24 20:10:21 +0000

    [add]画像フォルダの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b90809d401b3449314410df3ff7cf6657b44107a5c9001eb9f9216731d7e15446c5014b73adf8b7ee643d75b17dbdce7406460be5d8a8f7cfd9263e8c6abc250'
